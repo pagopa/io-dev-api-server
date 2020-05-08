70ba87eb190e9390d6fcde9c7a966c6ddfdf90cf
Matteo Boschi <mbmaken@gmail.com>
2020-05-08 10:43:30 +0200

    now message content is a complex markdown

--- src/payloads/message.ts
+++ src/payloads/message.ts
@@ -9,6 +9,51 @@ import { getRandomIntInRange, getRandomStringId } from "../../src/utils/id";
 import { validatePayload } from "../../src/utils/validator";
 import { IOResponse } from "./response";
 
+const messageMarkdown = `
+# blocco1 
+
+  ## blocco2 
+
+  ### blocco3 
+
+  #### blocco4
+
+  Test list: 
+  - item1
+  - item2 
+  - item3 
+  - item4 
+  - item5 
+  - item6 
+  
+È universalmente **riconosciuto** che un _lettore_ che **osserva** il layout di una pagina viene distratto dal contenuto testuale se questo è leggibile. Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quanto avviene se si utilizzano brevi frasi ripetute, ad esempio “testo qui”), apparendo come un normale blocco di testo leggibile. Molti software di impaginazione e di web design utilizzano Lorem Ipsum come testo modello. Molte versioni del testo sono state prodotte negli anni, a volte casualmente, a volte di proposito (ad esempio inserendo passaggi ironici).
+
+### link esterni
+
+[LINK ESTERNO GOOGLE](https://www.google.it)
+
+[LINK CORROTTO](google.it)
+
+### link interni
+
+[MESSAGES_HOME](ioit://MESSAGES_HOME)
+
+[PROFILE_PREFERENCES_HOME](ioit://PROFILE_PREFERENCES_HOME)
+
+[SERVICES_HOME](ioit://SERVICES_HOME)
+
+[PROFILE_MAIN](ioit://PROFILE_MAIN)
+
+[PROFILE_PRIVACY](ioit://PROFILE_PRIVACY)
+
+[PROFILE_PRIVACY_MAIN](ioit://PROFILE_PRIVACY_MAIN)
+
+[WALLET_HOME](ioit://WALLET_HOME)
+
+[WALLET_LIST](ioit://WALLET_LIST)
+
+[PAYMENTS_HISTORY_SCREEN](ioit://PAYMENTS_HISTORY_SCREEN)`;
+
 /**
  * generate a list containg count messages with the given fiscal_code
  * @param count the number of messages to generate
@@ -62,8 +107,7 @@ const createMessageWithContent = (
   return {
     content: {
       subject: `subject [${serviceId}]`,
-      markdown:
-        "test test test test test test test test test test test test test test test test test test test test test test test test test test",
+      markdown: messageMarkdown,
       due_date: date,
       payment_data: paymentData
     },
