f17234b14096c950f4df8cf5047ffd62b6fd5bc2
Matteo Boschi <mbmaken@gmail.com>
2020-05-21 14:55:01 +0200

    add very long link to message markdown content

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -17,6 +17,8 @@ export const messageMarkdown = `
   
 È universalmente **riconosciuto** che un _lettore_ che **osserva** il layout di una pagina viene distratto dal contenuto testuale se questo è leggibile. Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quanto avviene se si utilizzano brevi frasi ripetute, ad esempio “testo qui”), apparendo come un normale blocco di testo leggibile. Molti software di impaginazione e di web design utilizzano Lorem Ipsum come testo modello. Molte versioni del testo sono state prodotte negli anni, a volte casualmente, a volte di proposito (ad esempio inserendo passaggi ironici).
 
+| copia e incolla il seguente link: \`https://verylongurl.com/verylong_very_long_very_long_very_long_very_long_very_long_very_long_very_long_very_long_very_long_very_long_very_long_\`
+
 ### link esterni
 
 [LINK ESTERNO GOOGLE](https://www.google.it)
