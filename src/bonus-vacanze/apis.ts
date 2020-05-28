346f64058d1ed7877a1b6bfa021f45c5f5a8fd4b
Matteo Boschi <mbmaken@gmail.com>
2020-05-28 14:47:03 +0200

    add activations apis

--- src/bonus-vacanze/apis.ts
+++ src/bonus-vacanze/apis.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
+import { fromNullable } from "fp-ts/lib/Option";
 import * as fs from "fs";
 import { Second } from "italia-ts-commons/lib/units";
 import { uuidv4 } from "../utils/strings";
 export const bonusVacanze = Router();
 
 const qrCodeBonusVacanze = fs
-  .readFileSync("assets/imgs/bonus-vacanze/qr_code_bonus_vacanze.png")
+  .readFileSync("assets/imgs/bonus-vacanze/qr-mysecretcode.svg")
   .toString("base64");
 
-// get the list of all available bonuses
+enum BonusStatusEnum {
+  "ACTIVE" = "ACTIVE",
+  "CANCELLED" = "CANCELLED",
+  "FAILED" = "FAILED",
+  "CONSUMED" = "CONSUMED"
+}
+
+// get the list of all available bonus types
 bonusVacanze.get("/", (_, res) => {
   const bonuses = {
     items: [
@@ -51,53 +59,87 @@ bonusVacanze.get(`/can-activate/:id_bonus`, (_, res) => {
   */
 });
 
+// tslint:disable-next-line: no-let
+let idActivationBonus: string | undefined;
 // Get all IDs of the bonus activations requested by
 // the authenticated user or by any between his family member
 bonusVacanze.get(`/activations`, (_, res) => {
-  const activations = {
-    items: [
-      {
-        id: "ABC",
-        is_applicant: false
-      },
-      {
-        id: "DEF",
-        is_applicant: false
-      },
-      {
-        id: "GHJ",
-        is_applicant: false
-      }
-    ]
-  };
-  return res.json(activations);
+  fromNullable(idActivationBonus).foldL(
+    () => {
+      // No activation found.
+      res.sendStatus(404);
+    },
+    // List of bonus activations ID activated or consumed by the authenticated user
+    // or by any between his family members (former and actual)
+    () => res.json({ items: [{ id: idActivationBonus, is_applicant: true }] })
+  );
+});
+
+bonusVacanze.get(`/activations/:bonus_id`, (_, res) => {
+  fromNullable(idActivationBonus).foldL(
+    () => {
+      // No active bonus found.
+      res.sendStatus(404);
+    },
+    // List of bonus activations ID activated or consumed by the authenticated user
+    // or by any between his family members (former and actual)
+    () =>
+      res.json({
+        id: idActivationBonus,
+        applicant_fiscal_code: "SPNDNL80R11C522K",
+        code: "MYSECRETCODE",
+        qr_code: {
+          mime_type: "svg+xml",
+          base64_content: qrCodeBonusVacanze
+        },
+        max_amount: 50000,
+        max_tax_benefit: 3000,
+        updated_at: "2020-07-04T12:20:00.000Z",
+        status: BonusStatusEnum.ACTIVE
+      })
+  );
+});
+
+// Start bonus activation request procedure
+bonusVacanze.post(`/activations`, (_, res) => {
+  // if there is no previous activation -> Request accepted -> send back the created id
+  fromNullable(idActivationBonus).foldL(
+    () => {
+      idActivationBonus = uuidv4();
+      res.status(202).json({ id: idActivationBonus });
+    },
+    // Cannot activate a new bonus because another bonus related to this user was found.
+    () => res.sendStatus(409)
+  );
 });
 
 // tslint:disable-next-line: no-let
-let id_task: string | undefined;
+let idEligibilityRequest: string | undefined;
 // tslint:disable-next-line: no-let
 let firstRequestTime = 0;
 const responseIseeAfter = 8 as Second;
 // Get eligibility (ISEE) check information for user's bonus
 
-// util function to reset some data structures
+// since all these apis implements a specific flow, if you want re-run it
+// some vars must be cleaned
 export const resetBonusVacanze = () => {
-  id_task = undefined;
+  idEligibilityRequest = undefined;
   firstRequestTime = 0;
+  idActivationBonus = undefined;
 };
 
 // Start bonus eligibility check (ISEE)
 bonusVacanze.post("/eligibility", (_, res) => {
-  if (id_task) {
+  if (idEligibilityRequest) {
     // a task already exists because it has been requested
     // return conflict status
-    res.status(409).json({ id: id_task });
+    res.status(409).json({ id: idEligibilityRequest });
     return;
   }
   firstRequestTime = new Date().getTime();
-  id_task = uuidv4();
+  idEligibilityRequest = uuidv4();
   // first time return the id of the created task -> request accepted
-  res.status(202).json({ id: id_task });
+  res.status(202).json({ id: idEligibilityRequest });
 });
 
 bonusVacanze.get("/eligibility", (_, res) => {
@@ -123,17 +165,23 @@ bonusVacanze.get("/eligibility", (_, res) => {
     max_tax_benefit: 3000
   };
   // no task created, not-found
-  if (id_task === undefined) {
+  if (idEligibilityRequest === undefined) {
     res.sendStatus(404);
     return;
   }
   const elapsedTime = (new Date().getTime() - firstRequestTime) / 1000;
   // if elapsedTime is less than responseIseeAfter return pending status
   // first time return the id of the created task
-  if (id_task && elapsedTime < responseIseeAfter) {
+  if (idEligibilityRequest && elapsedTime < responseIseeAfter) {
     // request accepted, return the task id
-    res.status(202).json({ id: id_task });
+    res.status(202).json({ id: idEligibilityRequest });
     return;
+  }
+  enum EligibilityCheckStatusEnum {
+    "USER_NOT_FOUND" = "USER_NOT_FOUND",
+    "INELIGIBLE" = "INELIGIBLE",
+    "ISEE_NOT_FOUND" = "ISEE_NOT_FOUND",
+    "ELIGIBLE" = "ELIGIBLE"
   }
   // possible states
   // - USER_NOT_FOUND    User not found in INPS database
@@ -142,16 +190,18 @@ bonusVacanze.get("/eligibility", (_, res) => {
   // - INELIGIBLE        The user is not eligible for the bonus
   // Request processed
   // TODO we should mock also the ERROR case
-  res
-    .status(200)
-    .json({ ...eligibilityCheck, id: id_task, status: "ELIGIBLE" });
+  res.status(200).json({
+    ...eligibilityCheck,
+    id: idEligibilityRequest,
+    status: EligibilityCheckStatusEnum.ELIGIBLE
+  });
 });
 
 // Cancel bonus eligibility check procedure (avoids sending a push notification when done)
 bonusVacanze.delete("/eligibility", (_, res) => {
-  if (id_task) {
+  if (idEligibilityRequest) {
     // Request canceled.
-    res.status(200).json({ id: id_task });
+    res.status(200).json({ id: idEligibilityRequest });
     return;
   }
   // not found
