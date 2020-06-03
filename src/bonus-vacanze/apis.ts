4ac77d513e0c98940a55f2bdb03bc85ec7c3fcc5
Matteo Boschi <mbmaken@gmail.com>
2020-06-03 10:15:45 +0200

    update available bonuses payload

--- src/bonus-vacanze/apis.ts
+++ src/bonus-vacanze/apis.ts
@@ -26,19 +26,22 @@ bonusVacanze.get("/", (_, res) => {
     items: [
       {
         id_type: 1,
+        service_id: "01DBJNYDCT0Q5G0D0K7RFS2R2F",
         name: "Bonus Vacanze",
+        active: true,
         description: "descrizione bonus vacanze",
         valid_from: "2020-07-01T00:00:00.000Z",
-        valid_to: "2020-12-31T23:59:59.000Z",
+        valid_to: "2020-12-31T00:00:00.000Z",
         cover:
           "https://gdsit.cdn-immedia.net/2018/08/fff810d2e44464312e70071340fd92fc.jpg"
       },
       {
         id_type: 2,
         name: "Bonus che non esiste",
+        active: false,
         description: "descrizione bonus che non esiste",
         valid_from: "2020-05-25T00:00:00.000Z",
-        valid_to: "2020-06-01T23:59:59.000Z",
+        valid_to: "2020-06-01T00:00:00.000Z",
         cover:
           "https://www.pensionipertutti.it/wp-content/uploads/2020/05/bonus-600-euro-autonomi.jpg"
       }
