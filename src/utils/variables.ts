15ad770e373a092192df3c221be11e09450b5409
Matteo Boschi <mbmaken@gmail.com>
2020-08-05 12:48:26 +0200

    update message markdown content

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -53,44 +53,43 @@ en:
 ---`;
 
 export const messageMarkdown = `
-# blocco1 
+# H1 
 
-  ## blocco2 
+## H2 
 
-  ### blocco3 
+### H3 
 
-  #### blocco4
+#### H4
 
-  Test list: 
-  - item1
-  - item2 
-  - item3 
-  - item4 
-  - item5 
-  - item6 
-  
-  
-| Elemento | Stato | Commenti |
-| --- | --- | --- |
-| Pulsanti small |OK | Completato |
-| Pulsanti big | OK | Completato |
-| Icone header | ~ | Bloccato da https://www.pivotaltracker.com/n/projects/2048617/stories/173767813 per evitare conflitti
-| Tab lista | KO | Non è possibile aumentare solo l’area toccabile, è possibile aumentare l’altezza grafica di tutto il componente
-| Menu di navigazione |KO | Non è possibile aumentare solo l’area toccabile
-| Testo bianco su fondo acqua | OK | Completato |
-| Link | OK | Completato |
-| Titoli di pagina | OK |  Risolto in https://www.pivotaltracker.com/n/projects/2048617/stories/173767813
-| Font size | OK | Completato |
-  
+esempio di lista: 
+
+- item1
+- item2 
+- item3 
+- item4 
+- item5 
+- item6 
+
+
+esempio di numerata: 
+
+1. item1
+1. item2 
+1. item3 
+1. item4 
+1. item5 
+1. item6 
+
+
 È universalmente **riconosciuto** che un _lettore_ che **osserva** il layout di una pagina viene distratto dal contenuto testuale se questo è leggibile. Lo scopo dell’utilizzo del Lorem Ipsum è che offre una normale distribuzione delle lettere (al contrario di quanto avviene se si utilizzano brevi frasi ripetute, ad esempio “testo qui”), apparendo come un normale blocco di testo leggibile. Molti software di impaginazione e di web design utilizzano Lorem Ipsum come testo modello. Molte versioni del testo sono state prodotte negli anni, a volte casualmente, a volte di proposito (ad esempio inserendo passaggi ironici).
 
 | copia e incolla il seguente link: \`https://verylongurl.com/verylong_very_long_very_long_very_long_very_long_very_long_very_long_very_long_very_long_very_long_very_long_very_long_\`
 
 ### link esterni
 
-[LINK ESTERNO GOOGLE](https://www.google.it)
+il link deve **sempre** includere il protocollo (http:// o https://)
 
-[LINK CORROTTO](google.it)
+[DESCRIZIONE LINK](https://www.google.it)
 
 ### link interni
 
@@ -116,4 +115,5 @@ export const messageMarkdown = `
 
 [PAYMENTS_HISTORY_SCREEN](ioit://PAYMENTS_HISTORY_SCREEN)
 
-[LINK CORROTTO](ioit://WRONG&$)`;
+[LINK CORROTTO](ioit://WRONG&$)
+`;
