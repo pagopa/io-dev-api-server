11ad12324aa56cc684f815b67fb60c656cef67b5
Matteo Boschi <mbmaken@gmail.com>
2020-09-03 18:47:50 +0200

    fixes

--- src/routers/misc.ts
+++ src/routers/misc.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { installHandler } from "../payloads/response";
+import { installCustomHandler } from "../payloads/response";
 import { sendFile } from "../utils/file";
 
 export const miscRouter = Router();
 
-installHandler(miscRouter, "get", "/test-cookies.html", (_, res) => {
+installCustomHandler(miscRouter, "get", "/test-cookies.html", (_, res) => {
   sendFile("assets/html/test_cookies.html", res);
-  return null;
 });
