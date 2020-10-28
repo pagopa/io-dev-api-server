7b26158d6498d37da9fe8c38ff6ee5fe4a518fa9
Matteo Boschi <mbmaken@gmail.com>
2020-10-28 18:23:46 +0100

    support award periods API

--- src/routers/features/bdp/award.ts
+++ src/routers/features/bdp/award.ts
@@ -1,19 +1,54 @@
 // return the T&C as a HTML string
-import { installCustomHandler } from "../../../payloads/response";
-import { addBPDPrefix } from "./index";
 import { Router } from "express";
+import fs from "fs";
+import { BpdAwardPeriods } from "../../../../generated/definitions/bpd/award/BpdAwardPeriods";
 import { assetsFolder } from "../../../global";
+import {
+  installCustomHandler,
+  installHandler
+} from "../../../payloads/response";
 import { listDir } from "../../../utils/file";
+import { toPayload } from "../../../utils/validator";
+import { addBPDPrefix } from "./index";
 
 export const bpdAward = Router();
+const readPeriodPreset = (fileName: string) =>
+  JSON.parse(
+    fs.readFileSync(assetsFolder + "/bpd/award/periods/" + fileName).toString()
+  );
+// tslint:disable-next-line: no-let
+let awardPeriods: BpdAwardPeriods = BpdAwardPeriods.decode(
+  readPeriodPreset("default.json")
+).getOrElse([]);
 
+// return the json file names to populate the dashboard UI
 installCustomHandler(
   bpdAward,
   "get",
-  addBPDPrefix("/award-periods"),
+  addBPDPrefix("/award/periods/presets"),
   (_, res) => {
-    const files = listDir(assetsFolder + "/bpd/award");
-    console.log(files);
+    const files = listDir(assetsFolder + "/bpd/award/periods");
+    res.json(files);
+  }
+);
+
+// update the period preset
+installCustomHandler(
+  bpdAward,
+  "post",
+  addBPDPrefix("/award/periods/presets/:period"),
+  (req, res) => {
+    const files = listDir(assetsFolder + "/bpd/award/periods");
+    const file = files.find(f => f === req.params.period);
+    if (file) {
+      awardPeriods = BpdAwardPeriods.decode(readPeriodPreset(file)).getOrElse(
+        []
+      );
+    }
     res.sendStatus(200);
   }
+);
+
+installHandler(bpdAward, "get", addBPDPrefix("/io/award-periods"), () =>
+  toPayload(awardPeriods)
 );
