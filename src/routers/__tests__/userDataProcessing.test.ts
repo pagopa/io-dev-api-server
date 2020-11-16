33133658c04fc2b2fc259aca8feec4d2458ffb2c
Simone <simone.biffi.91@gmail.com>
2020-11-16 17:56:33 +0100

    add test for DELETE userDataProcessing API

--- src/routers/__tests__/userDataProcessing.test.ts
+++ src/routers/__tests__/userDataProcessing.test.ts
@@ -18,6 +18,14 @@ it("info should return ProblemJson with not found", async done => {
   done();
 });
 
+it("Delete should return conflict error if DELETE status is undefined", async done => {
+  const response = await request.delete(
+    `${basePath}/user-data-processing/${UserDataProcessingChoiceEnum.DELETE}`
+  );
+  expect(response.status).toBe(409);
+  done();
+});
+
 it("Post should create a pending operation", async done => {
   const response = await request
     .post(`${basePath}/user-data-processing`)
@@ -32,5 +40,41 @@ it("Post should create a pending operation", async done => {
   const sr = UserDataProcessing.decode(response.body);
   expect(sr.isRight()).toBeTruthy();
   expect(sr.value).toEqual(pending);
+  done();
+});
+
+it("Delete should set the request as aborted if the choice is DELETE and status is PENDING", async done => {
+  const response = await request.delete(
+    `${basePath}/user-data-processing/${UserDataProcessingChoiceEnum.DELETE}`
+  );
+  expect(response.status).toBe(202);
+
+  const newStatus = await request.get(
+    `${basePath}/user-data-processing/${UserDataProcessingChoiceEnum.DELETE}`
+  );
+  const aborted: UserDataProcessing = {
+    choice: UserDataProcessingChoiceEnum.DELETE,
+    status: UserDataProcessingStatusEnum.ABORTED,
+    version: 1
+  };
+  const sr = UserDataProcessing.decode(newStatus.body);
+  expect(sr.isRight()).toBeTruthy();
+  expect(sr.value).toEqual(aborted);
+  done();
+});
+
+it("Delete should return conflict error if choice is DOWNLOAD", async done => {
+  const response = await request.delete(
+    `${basePath}/user-data-processing/${UserDataProcessingChoiceEnum.DOWNLOAD}`
+  );
+  expect(response.status).toBe(409);
+  done();
+});
+
+it("Delete should return conflict error if DELETE status is ABORTED", async done => {
+  const response = await request.delete(
+    `${basePath}/user-data-processing/${UserDataProcessingChoiceEnum.DELETE}`
+  );
+  expect(response.status).toBe(409);
   done();
 });
