26b7a184b07c733e50a99e3da4d58796b815b1ae
Matteo Boschi <mbmaken@gmail.com>
2020-11-20 13:11:58 +0100

    api support token

--- src/routers/session.ts
+++ src/routers/session.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import * as faker from "faker/locale/it";
 import { installHandler } from "../payloads/response";
 import { session } from "../payloads/session";
 import { addApiV1Prefix } from "../utils/strings";
-
+import { toPayload } from "../utils/validator";
 export const sessionRouter = Router();
 
 installHandler(sessionRouter, "get", addApiV1Prefix("/session"), _ => session);
+
+// TODO replace with the relative generated codec when all specs will be updated
+installHandler(sessionRouter, "get", addApiV1Prefix("/token"), _ =>
+  toPayload({
+    access_token: faker.random.uuid(),
+    expires_in: faker.random.number()
+  })
+);
