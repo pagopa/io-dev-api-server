f7599542277c4321d43881dceb408c3f768a832a
Fabrizio <fartux@gmail.com>
2020-11-23 18:44:18 +0100

    change numberObfuscated for bpay

--- src/payloads/wallet_v2.ts
+++ src/payloads/wallet_v2.ts
@@ -75,7 +75,7 @@ export const generateBancomatPay = (count: number): ReadonlyArray<BPayInfo> => {
   return range(1, count).map(_ => ({
     bankName: faker.company.companyName(),
     instituteCode: config.index.toString(),
-    numberObfuscated: "*".repeat(4) + config.index.toString().padStart(4, "0"),
+    numberObfuscated: "+3934"+ "*".repeat(7) + config.index.toString().padStart(3, "0"),
     paymentInstruments: [],
     uidHash
   }));
