b2c48b037188bab5b4942371c119ba92e211333f
Matteo Boschi <mbmaken@gmail.com>
2021-02-09 19:54:34 +0100

    fix lint errors

--- src/global.ts
+++ src/global.ts
@@ -8,8 +8,5 @@ export const staticContentRootPath = "/static_contents";
 export const assetsFolder = path.resolve(".") + "/assets";
 export const globalDelay = 0 as Millisecond;
 export const shouldShuffle = false;
-
-
 // services
-
 export const servicesNumber = 15;
