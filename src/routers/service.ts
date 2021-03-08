2edf704e308fb5092fad1e3adc0db79a2bbbdd1b
Matteo Boschi <mbmaken@gmail.com>
2021-03-08 11:33:14 +0100

    add html services sandbox

--- src/routers/service.ts
+++ src/routers/service.ts
@@ -8,6 +8,9 @@ import {
   getServicesTuple
 } from "../payloads/service";
 import { addApiV1Prefix } from "../utils/strings";
+import { loginWithToken } from "../payloads/login";
+import { sendFile } from "../utils/file";
+import { publicRouter } from "./public";
 
 export const serviceRouter = Router();
 export const services = getServices(servicesNumber);
@@ -31,3 +34,12 @@ addHandler(
     res.json(service || notFound.payload);
   }
 );
+
+/**
+ * just for test purposes
+ * an html test page to trigger the services webview
+ * see https://www.pivotaltracker.com/story/show/177226606
+ */
+addHandler(publicRouter, "get", "/services_web_view", (req, res) => {
+  sendFile("assets/html/services_web_view.html", res);
+});
