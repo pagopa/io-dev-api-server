7fb0c033882871d83b5e49de549b5a174b2b783f
Simone <simone.biffi.91@gmail.com>
2021-07-12 17:20:26 +0200

    add regioni api

--- src/routers/features/siciliaVola/unsecured/index.ts
+++ src/routers/features/siciliaVola/unsecured/index.ts
@@ -8,6 +8,16 @@ export const unsecuredSvRouter = Router();
 
 const addPrefix = (path: string) => addApiV1Prefix(`/sv/rest/unsecured${path}`);
 
+/**
+ * Get the states list
+ */
 addHandler(unsecuredSvRouter, "get", addPrefix("/statiUE"), (_, res) =>
   res.json(readFileAsJSON(assetsFolder + "/siciliaVola/states.json"))
+);
+
+/**
+ * Get the region list
+ */
+addHandler(unsecuredSvRouter, "get", addPrefix("/regioni"), (_, res) =>
+  res.json(readFileAsJSON(assetsFolder + "/siciliaVola/regions.json"))
 );
