0e286872efe98e3d84e39d23d22cd36e253b4fff
Matteo Boschi <mbmaken@gmail.com>
2021-07-27 15:27:57 +0200

    add tests

--- src/routers/__tests__/wallets.test.ts
+++ src/routers/__tests__/wallets.test.ts
@@ -1,8 +1,10 @@
 import supertest, { Response } from "supertest";
+import { WalletPaymentstatus } from "../../../generated/definitions/pagopa/WalletPaymentstatus";
 import { Psp } from "../../../generated/definitions/pagopa/walletv2/Psp";
 import { SessionResponse } from "../../../generated/definitions/pagopa/walletv2/SessionResponse";
 import { TransactionListResponse } from "../../../generated/definitions/pagopa/walletv2/TransactionListResponse";
 import { WalletListResponse } from "../../../generated/definitions/pagopa/walletv2/WalletListResponse";
+import { WalletV2ListResponse } from "../../../generated/definitions/pagopa/walletv2/WalletV2ListResponse";
 import { sessionToken } from "../../payloads/wallet";
 import app from "../../server";
 import {
@@ -20,6 +22,16 @@ const appendWallet2Prefix = (path: string) => `${walletV2Path}${path}`;
 const testGetWallets = (response: Response) => {
   expect(response.status).toBe(200);
   const wallets = WalletListResponse.decode(response.body);
+  expect(wallets.isRight()).toBeTruthy();
+  if (wallets.isRight() && wallets.value.data) {
+    expect(wallets.value.data.length).toBe(walletCount);
+  }
+  return wallets.value;
+};
+
+const testGetWalletsV2 = (response: Response) => {
+  expect(response.status).toBe(200);
+  const wallets = WalletV2ListResponse.decode(response.body);
   expect(wallets.isRight()).toBeTruthy();
   if (wallets.isRight() && wallets.value.data) {
     expect(wallets.value.data.length).toBe(walletCount);
@@ -54,6 +66,32 @@ it("should set a wallet as favourite", async done => {
     appendWalletPrefix(`/wallet/${firstWallet.idWallet}/actions/favourite`)
   );
   expect(response.status).toBe(200);
+  done();
+});
+
+it("should set pagoPa to false", async done => {
+  const responseWallets = await request.get(appendWallet2Prefix("/wallet"));
+  const wallets: any = testGetWallets(responseWallets);
+  const firstWallet = wallets.data[0];
+  const response = await request
+    .put(appendWalletPrefix(`/wallet/${firstWallet.idWallet}/payment-status`))
+    .send({ pagoPA: false });
+  expect(response.status).toBe(200);
+  const responsePayload = WalletPaymentstatus.decode(response.body);
+  expect(responsePayload.isRight()).toBeTruthy();
+  if (responsePayload.isRight()) {
+    expect(responsePayload.value).toEqual({ pagoPA: false });
+  }
+  // invert
+  const responseInvert = await request
+    .put(appendWalletPrefix(`/wallet/${firstWallet.idWallet}/payment-status`))
+    .send({ pagoPA: true });
+  expect(responseInvert.status).toBe(200);
+  const responsePayloadInvert = WalletPaymentstatus.decode(responseInvert.body);
+  expect(responsePayloadInvert.isRight()).toBeTruthy();
+  if (responsePayloadInvert.isRight()) {
+    expect(responsePayloadInvert.value).toEqual({ pagoPA: true });
+  }
   done();
 });
 
