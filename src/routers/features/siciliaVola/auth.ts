4f83d7faa5db5776c85d4eda0a669df0a4a122d8
Simone <simone.biffi.91@gmail.com>
2021-07-29 09:54:32 +0200

    add auth token generation api

---
+++ src/routers/features/siciliaVola/auth.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import * as faker from "faker";
+import { addHandler } from "../../../payloads/response";
+import { addApiV1Prefix } from "../../../utils/strings";
+import { securedSvRouter } from "./secured";
+
+export const authSvRouter = Router();
+
+const addPrefix = (path: string) => addApiV1Prefix(`/mitvoucher/auth${path}`);
+
+/**
+ * Get the mit auth token
+ */
+addHandler(securedSvRouter, "get", addPrefix("/token"), (_, res) =>
+  res.json({ token: faker.random.uuid() })
+);
