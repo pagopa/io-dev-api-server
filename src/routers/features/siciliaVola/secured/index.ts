35c56d0ac35599016a06c554a7970fcbe03c5bfa
Simone <simone.biffi.91@gmail.com>
2021-07-29 09:54:46 +0200

    add secured api

--- src/routers/features/siciliaVola/secured/index.ts
+++ src/routers/features/siciliaVola/secured/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { assetsFolder } from "../../../../global";
-import { getVouchersBeneficiary } from "../../../../payloads/features/siciliaVola";
+import * as t from "io-ts";
+import { ListaVoucherBeneficiarioOutputBean } from "../../../../../generated/definitions/siciliaVola/ListaVoucherBeneficiarioOutputBean";
+import {
+  getAereoportiSede,
+  getVouchersBeneficiary
+} from "../../../../payloads/features/siciliaVola";
 import { addHandler } from "../../../../payloads/response";
-import { readFileAsJSON } from "../../../../utils/file";
 import { addApiV1Prefix } from "../../../../utils/strings";
 
 export const securedSvRouter = Router();
@@ -10,12 +13,52 @@ export const securedSvRouter = Router();
 const addPrefix = (path: string) =>
   addApiV1Prefix(`/mitvoucher/data/rest/secured${path}`);
 
+// tslint:disable-next-line: no-let prefer-const
+let vouchersBeneficiary: ListaVoucherBeneficiarioOutputBean = getVouchersBeneficiary(
+  5
+);
+
 /**
- * Get the states list
+ * Get the voucher list
  */
 addHandler(
   securedSvRouter,
   "post",
   addPrefix("/beneficiario/ricercaVoucher"),
-  (_, res) => res.json(getVouchersBeneficiary(5))
+  (_, res) => res.json(vouchersBeneficiary)
+);
+
+/**
+ * Get the airport destination list
+ */
+addHandler(
+  securedSvRouter,
+  "get",
+  addPrefix("/beneficiario/aeroportiSede/:region_id"),
+  (_, res) => res.json(getAereoportiSede(5))
+);
+
+/**
+ * Delete a voucher
+ */
+addHandler(
+  securedSvRouter,
+  "get",
+  addPrefix("/beneficiario/annullaVoucher"),
+  (req, res) => {
+    const { voucherId } = req.body;
+    if (t.Integer.decode(voucherId).isLeft()) {
+      // validate the body value
+      res.sendStatus(500);
+      return;
+    }
+
+    if (vouchersBeneficiary.listaRisultati) {
+      vouchersBeneficiary.listaRisultati = vouchersBeneficiary.listaRisultati.filter(
+        v => v.idVoucher !== voucherId
+      );
+    }
+    res.sendStatus(200);
+    return;
+  }
 );
