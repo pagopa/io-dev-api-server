038485ffbdcb0a58d6364e36a93eb4ac6e26270d
Matteo Boschi <mbmaken@gmail.com>
2021-11-04 18:32:41 +0100

    add psp search api

---
+++ src/routers/walletV3/methods/paypal.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { PaypalPspListResponse } from "../../../../generated/definitions/pagopa/PaypalPspListResponse";
+import { assetsFolder } from "../../../config";
+import { addHandler } from "../../../payloads/response";
+import { readFileAsJSON } from "../../../utils/file";
+import { validatePayload } from "../../../utils/validator";
+import { appendWalletV3Prefix } from "../../../utils/wallet";
+
+export const payPalRouter = Router();
+/**
+ * return the list of the paypal psp list
+ */
+addHandler(
+  payPalRouter,
+  "get",
+  appendWalletV3Prefix("/paypal/searchPSP"),
+  (_, res) => {
+    const maybePspResponse = validatePayload(
+      PaypalPspListResponse,
+      readFileAsJSON(assetsFolder + "/pm/paypal/psp.json")
+    );
+    res.json(maybePspResponse);
+  }
+);
