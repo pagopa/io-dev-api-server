1f9e1ef9fb00cf7324304e70b54497e1b12892d1
Simone <simone.biffi.91@gmail.com>
2021-12-10 11:11:56 +0100

    update data

--- src/payloads/zendesk.ts
+++ src/payloads/zendesk.ts
@@ -7,27 +7,42 @@ export const zendeskConfig: Zendesk = {
     categories: [
       {
         value: "Certificazione Verde COVID-19",
-        "it-IT": "Certificazione Verde COVID-19",
-        "en-EN": "COVID-19 Green Certification",
+        description: {
+          "it-IT": "Certificazione Verde COVID-19",
+          "en-EN": "COVID-19 Green Certification"
+        },
         zendeskSubCategories: {
           id: "360027411097",
           subCategories: [
             {
               value: "Informazioni generali",
-              "it-IT": "Informazioni generali",
-              "en-EN": "General informations"
+              description: {
+                "it-IT": "Informazioni generali",
+                "en-EN": "General informations"
+              }
             },
             {
               value: "Non ho ricevuto nulla",
-              "it-IT": "Non ho ricevuto nulla",
-              "en-EN": "I have not received anything"
+              description: {
+                "it-IT": "Non ho ricevuto nulla",
+                "en-EN": "I have not received anything"
+              }
             },
             {
               value: "Problemi tecnici su IO",
-              "it-IT": "Problemi tecnici su IO",
-              "en-EN": "Technical problems on IO"
+              description: {
+                "it-IT": "Problemi tecnici su IO",
+                "en-EN": "Technical problems on IO"
+              }
             }
           ]
+        }
+      },
+      {
+        value: "Generali",
+        description: {
+          "it-IT": "Generali",
+          "en-EN": "General"
         }
       }
     ]
