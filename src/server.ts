d8d2cfa920e97d27a1009bc951709dc7124480c2
Luca Cavallaro <luca.cavallaro@pagopa.it>
2021-12-24 11:20:23 +0100

    move routers in start.ts

--- src/server.ts
+++ src/server.ts
@@ -1,94 +1,21 @@
-import { Millisecond } from "@pagopa/ts-commons/lib/units";
+import express, { Router } from "express";
 import bodyParser from "body-parser";
-import express from "express";
 import morgan from "morgan";
-
-import { bpd } from "./routers/features/bdp";
-import { bpdAward } from "./routers/features/bdp/award";
-import { bpdRanking } from "./routers/features/bdp/ranking/v1";
-import { bpdRankingV2 } from "./routers/features/bdp/ranking/v2";
-import { bpdWinningTransactionsV1 } from "./routers/features/bdp/winning-transactions/v1";
-import { bpdWinningTransactionsV2 } from "./routers/features/bdp/winning-transactions/v2";
-import { bonusVacanze } from "./routers/features/bonus-vacanze";
-import { cgnRouter } from "./routers/features/cgn";
-import { cgnGeoRouter } from "./routers/features/cgn/geocoding";
-import { cgnMerchantsRouter } from "./routers/features/cgn/merchants";
-import { euCovidCertRouter } from "./routers/features/eu_covid_cert";
-import { svRouter } from "./routers/features/siciliaVola";
-import { messageRouter } from "./routers/message";
-import { miscRouter } from "./routers/misc";
-import { paymentRouter } from "./routers/payment";
-import { profileRouter } from "./routers/profile";
-import { publicRouter } from "./routers/public";
-import { serviceRouter } from "./routers/service";
-import { servicesMetadataRouter } from "./routers/services_metadata";
-import { sessionRouter } from "./routers/session";
-import { walletRouter } from "./routers/wallet";
-import { wallet2Router } from "./routers/walletsV2";
-import { dashboardWalletV2Router } from "./routers/walletsV2/configDashboard";
-import { bancomatRouter } from "./routers/walletsV2/methods/bancomat";
-import { bpayRouter } from "./routers/walletsV2/methods/bpay";
-import { cobadgeRouter } from "./routers/walletsV2/methods/cobadge";
-import { satispayRouter } from "./routers/walletsV2/methods/satispay";
-import { payPalRouter } from "./routers/walletsV3/methods/paypal";
-import { delayer } from "./utils/delay_middleware";
-
 import * as O from "fp-ts/lib/Option";
 
-/*// create express server
-const app: Application = express();
-// parse body request as json
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// set middlewares
-// if you want to add a delay in your server, use a global delayer (utils/delay_middleware)
-app.use(delayer(ioDevServerConfig.global.delay as Millisecond));
-// set middleware logger
-app.use(
-  morgan(
-    ":date[iso] :method :url :status :res[content-length] - :response-time ms"
-  )
-);*/
-
-const routers = [
-  publicRouter,
-  profileRouter,
-  sessionRouter,
-  messageRouter,
-  serviceRouter,
-  walletRouter,
-  wallet2Router,
-  satispayRouter,
-  payPalRouter,
-  bpayRouter,
-  bancomatRouter,
-  cobadgeRouter,
-  dashboardWalletV2Router,
-  paymentRouter,
-  servicesMetadataRouter,
-  bonusVacanze,
-  miscRouter,
-  bpd,
-  bpdAward,
-  bpdRanking,
-  bpdRankingV2,
-  bpdWinningTransactionsV1,
-  bpdWinningTransactionsV2,
-  cgnRouter,
-  cgnMerchantsRouter,
-  cgnGeoRouter,
-  euCovidCertRouter,
-  svRouter
-];
+import { Millisecond } from "@pagopa/ts-commons/lib/units";
+import { delayer } from "./utils/delay_middleware";
 
 type CreateMockServerOptions = {
   delay: O.Option<Millisecond>;
   logger: boolean;
+  routers: ReadonlyArray<Router>;
 }
 
 export const defaultMockServerOptions: CreateMockServerOptions = {
   delay: O.none,
-  logger: false
+  logger: false,
+  routers: []
 }
 
 export const createMockServer = (options = defaultMockServerOptions) => {
@@ -100,6 +27,6 @@ export const createMockServer = (options = defaultMockServerOptions) => {
   if (options.logger) {
     app.use(morgan(":date[iso] :method :url :status :res[content-length] - :response-time ms"));
   }
-  routers.forEach(r => app.use)
+  options.routers.forEach(r => app.use(r));
   return app;
 };
--- src/start.ts
+++ src/start.ts
@@ -10,12 +10,72 @@ import { readFileAsJSON } from "./utils/file";
 import { interfaces, serverHostname, serverPort } from "./utils/server";
 import * as O from "fp-ts/lib/Option";
 import { Millisecond } from "@pagopa/ts-commons/lib/units";
+
+import { bpd } from "./routers/features/bdp";
+import { bpdAward } from "./routers/features/bdp/award";
+import { bpdRanking } from "./routers/features/bdp/ranking/v1";
+import { bpdRankingV2 } from "./routers/features/bdp/ranking/v2";
+import { bpdWinningTransactionsV1 } from "./routers/features/bdp/winning-transactions/v1";
+import { bpdWinningTransactionsV2 } from "./routers/features/bdp/winning-transactions/v2";
+import { bonusVacanze } from "./routers/features/bonus-vacanze";
+import { cgnRouter } from "./routers/features/cgn";
+import { cgnGeoRouter } from "./routers/features/cgn/geocoding";
+import { cgnMerchantsRouter } from "./routers/features/cgn/merchants";
+import { euCovidCertRouter } from "./routers/features/eu_covid_cert";
+import { svRouter } from "./routers/features/siciliaVola";
+import { messageRouter } from "./routers/message";
+import { miscRouter } from "./routers/misc";
+import { paymentRouter } from "./routers/payment";
+import { profileRouter } from "./routers/profile";
+import { publicRouter } from "./routers/public";
+import { serviceRouter } from "./routers/service";
+import { servicesMetadataRouter } from "./routers/services_metadata";
+import { sessionRouter } from "./routers/session";
+import { walletRouter } from "./routers/wallet";
+import { wallet2Router } from "./routers/walletsV2";
+import { dashboardWalletV2Router } from "./routers/walletsV2/configDashboard";
+import { bancomatRouter } from "./routers/walletsV2/methods/bancomat";
+import { bpayRouter } from "./routers/walletsV2/methods/bpay";
+import { cobadgeRouter } from "./routers/walletsV2/methods/cobadge";
+import { satispayRouter } from "./routers/walletsV2/methods/satispay";
+import { payPalRouter } from "./routers/walletsV3/methods/paypal";
+
 // read package.json to print some info
 const packageJson = readFileAsJSON("./package.json");
 
 const app = createMockServer({
   delay: O.some(ioDevServerConfig.global.delay as Millisecond),
   logger: true,
+  routers: [
+    publicRouter,
+    profileRouter,
+    sessionRouter,
+    messageRouter,
+    serviceRouter,
+    walletRouter,
+    wallet2Router,
+    satispayRouter,
+    payPalRouter,
+    bpayRouter,
+    bancomatRouter,
+    cobadgeRouter,
+    dashboardWalletV2Router,
+    paymentRouter,
+    servicesMetadataRouter,
+    bonusVacanze,
+    miscRouter,
+    bpd,
+    bpdAward,
+    bpdRanking,
+    bpdRankingV2,
+    bpdWinningTransactionsV1,
+    bpdWinningTransactionsV2,
+    cgnRouter,
+    cgnMerchantsRouter,
+    cgnGeoRouter,
+    euCovidCertRouter,
+    svRouter
+  ]
 });
 
 app.listen(serverPort, serverHostname, async () => {
