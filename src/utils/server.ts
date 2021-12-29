b77a898017d4f90daa336ad6f1a100c1b4ff2977
Luca Cavallaro <luca.cavallaro@pagopa.it>
2021-12-29 15:20:19 +0100

    refactor: remove monadic parameter form getIpv4Addresses

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -6,21 +6,17 @@ export const serverHostname = "0.0.0.0"; // public
 
 const interestingNetworkInterfaces = new Set(["en0", "eth0"]);
 
-const getIpv4Addresses = (
-  allNets: O.Option<ReadonlyArray<os.NetworkInterfaceInfo>>
-) =>
-  allNets.foldL(
-    () => [],
-    nets => nets.filter(net => net.family === "IPv4" && !net.internal)
-  );
+const getIpv4Addresses = (nets: ReadonlyArray<os.NetworkInterfaceInfo>) =>
+  nets.filter(net => net.family === "IPv4" && !net.internal);
 
 export const interfaces = Object.entries(os.networkInterfaces())
   .filter(([name]) => interestingNetworkInterfaces.has(name))
   .reduce(
     (netInteraces, [name, nets]) => {
-      const addresses = getIpv4Addresses(
-        O.fromNullable(nets)
-      ).map(({ address }) => ({ name, address }));
+      const addresses = O.fromNullable(nets)
+        .map(getIpv4Addresses)
+        .getOrElseL(() => [])
+        .map(({ address }) => ({ name, address }));
       return [...netInteraces, ...addresses];
     },
     [
