4d46a207b350e16b0fa1ab334b97109cdc75e745
Fabrizio <26501317+fabriziofff@users.noreply.github.com>
2022-01-31 15:07:22 +0100

    fix import

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,7 +1,7 @@
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import { Response } from "express";
-import { pipe } from "fp-ts/es6/pipeable";
 import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/pipeable";
 import fs from "fs";
 import { Validation } from "io-ts";
 
