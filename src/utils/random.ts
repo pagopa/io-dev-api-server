cc71e1d0a12483f4ca0871e9f060c6c4a5323817
Luca Cavallaro <luca.cavallaro@pagopa.it>
2022-02-03 11:22:02 +0100

    temp: disable random value function

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,5 +1,6 @@
-import { ioDevServerConfig } from "../config";
-import { AllorRandomValueKeys } from "../types/config";
+// TODO: support random values
+
+import { AllorRandomValueKeys } from "../io-dev/config";
 
 type RandomValueFunc = <T>(
   defaultValue: T,
@@ -10,22 +11,9 @@ type RandomValueFunc = <T>(
 const getValueGlobalRandomOff: RandomValueFunc = <T>(defaultValue: T) =>
   defaultValue;
 
-const getValueGlobalRandomOn: RandomValueFunc = <T>(
-  defaultValue: T,
-  randomValue: T,
-  configSectionKey: AllorRandomValueKeys
-) =>
-  ioDevServerConfig[configSectionKey].allowRandomValues ||
-  defaultValue === undefined
-    ? randomValue
-    : defaultValue;
-
 /**
  * if global random (allowRandomValues) is OFF this will be a function that will always return the default value
  * otherwise it will be a function that checks if random is enabled for that specific section (if it is omitted if, random is allowed by default)
  * if the specific random section is enabled then the random value will be returned, the default value otherwise
  */
-export const getRandomValue: RandomValueFunc = ioDevServerConfig.global
-  .allowRandomValues
-  ? getValueGlobalRandomOn
-  : getValueGlobalRandomOff;
+export const getRandomValue: RandomValueFunc = getValueGlobalRandomOff;
