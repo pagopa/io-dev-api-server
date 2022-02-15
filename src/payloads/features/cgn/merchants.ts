751d333a66c9dbb0afde1b4a0d3be6067fac6607
Matteo Boschi <mbmaken@gmail.com>
2022-02-15 18:19:44 +0100

    update

--- src/payloads/features/cgn/merchants.ts
+++ src/payloads/features/cgn/merchants.ts
@@ -18,7 +18,7 @@ import {
   ProductCategoryEnum
 } from "../../../../generated/definitions/cgn/merchants/ProductCategory";
 import { getRandomValue } from "../../../utils/random";
-import { serverIpv4Address, serverPort } from "../../../utils/server";
+import { serverUrl } from "../../../utils/server";
 
 const availableCategories: ReadonlyArray<ProductCategory> = [
   ProductCategoryEnum.cultureAndEntertainment,
@@ -102,7 +102,7 @@ export const offlineMerchants: OfflineMerchants = {
   })
 };
 
-const discountUrl = `http://${serverIpv4Address}:${serverPort}/merchant_landing` as Discount["discountUrl"];
+const discountUrl = `http://${serverUrl}/merchant_landing` as Discount["discountUrl"];
 
 const generateDiscountMethod = (discountCodeType: DiscountCodeTypeEnum) => {
   switch (discountCodeType) {
