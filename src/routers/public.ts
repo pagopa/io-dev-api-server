9ee9283891d9e4138dd58fd246f3dfbfcab6590d
Cristiano Tofani <cri.tofani@gmail.com>
2023-02-02 12:08:01 +0100

    add a check

--- src/routers/public.ts
+++ src/routers/public.ts
@@ -6,6 +6,7 @@ import { Router } from "express";
 import * as faker from "faker";
 import * as E from "fp-ts/lib/Either";
 import * as jose from "jose";
+import { parseStringPromise } from "xml2js";
 import * as zlib from "zlib";
 import { assetsFolder, ioDevServerConfig } from "../config";
 import { backendInfo } from "../payloads/backend";
@@ -28,6 +29,29 @@ export const publicRouter = Router();
 
 export const DEFAULT_LOLLIPOP_HASH_ALGORITHM = "sha256";
 
+let thumbprint: string;
+
+const test = async () => {
+  const samlReq =
+    "rVRNb5tAEL3nV1jc%2BTBpq2pliFxbbaicltpOD7mtYUjWhl28M9hOfn0XTB1HxU4VhQtoefvmzcybGVztiry3AY1CycDqO551FV4MkBd5yYYVPcgprCtA6hmcRNb8CKxKS6Y4CmSSF4CMEjYb3kyY73is1IpUonLrotfxROPAur5D%2BjyWK%2BTfbIXL1fpjxjfR90nuDePs5umJZ%2BulqlKu%2FKib5PdfvSbeiTCIFUQSiUsyMM%2B%2FtL2%2B7Xvzvse8S%2FbBdz553l333bhN4IuQqZD357Nd7EHIrufz2I5%2FzubdpGNTQyE5NbIfiEpkrlsqJBCp07wdQe5SpXaGLqJC0BuRgIZUaEiom%2FSr0gk0XQos0hV0o4ZoyOrAIyWxKkDP9ty308mzFF6W9oInK5CpI5TRwnPBa0n8xO0TsYi0WFQEe7SpTAuPZAq7wDrRrrA5bWzHmtbpI7%2Bdb8BBXwfzD4P%2BVZlMMgH69Vw7GEyFC07nFdQnwvStgTKQJOixgyp8LfzAPcr%2FqCIlq%2FOIxrHKRfL4XoP4tuxIc4nC5Hia1H0hvV0fkDY2Na4g2L3bMnnxjFRRci2wHjBjPFFUxX%2FcCg%2BQvfuOZY5yY64pZG%2Fw4qHZ2%2B3WwdLM%2BL3a1OM0M98Tv211V7C2eu6Z8pkF7f67ocM%2F";
+
+  const decoded = decodeURIComponent(samlReq);
+  const deflated = zlib
+    .inflateRawSync(Buffer.from(decoded, "base64"))
+    .toString();
+
+  console.log(deflated);
+
+  const xmlToJson = await parseStringPromise(deflated);
+
+  const authnRequest = xmlToJson["samlp:AuthnRequest"];
+
+  if (authnRequest) {
+    console.log(authnRequest.$.ID);
+    console.log(thumbprint);
+  }
+};
+
 addHandler(publicRouter, "get", "/login", async (req, res) => {
   if (
     req.headers["x-pagopa-lollipop-pub-key"] &&
@@ -37,12 +61,12 @@ addHandler(publicRouter, "get", "/login", async (req, res) => {
       req.headers["x-pagopa-lollipop-pub-key"] as string
     );
     if (E.isRight(jwkPK) && JwkPublicKey.is(jwkPK.right)) {
-      const pkHash = await jose.calculateJwkThumbprint(
+      thumbprint = await jose.calculateJwkThumbprint(
         jwkPK.right,
         DEFAULT_LOLLIPOP_HASH_ALGORITHM
       );
 
-      const samlRequest = getSamlRequest(pkHash);
+      const samlRequest = getSamlRequest(thumbprint);
 
       const authNRequest = zlib.deflateRawSync(samlRequest).toString("base64");
 
@@ -68,6 +92,7 @@ addHandler(publicRouter, "get", "/login", async (req, res) => {
 });
 
 addHandler(publicRouter, "get", "/idp-login", (req, res) => {
+  test();
   if (req.query.authorized === "1" || ioDevServerConfig.global.autoLogin) {
     res.redirect(loginWithToken);
     return;
