d60ac4bfa48882aad5f223b5e9e08a6fddcad2e8
Federico Mastrini <federicomastrini93@gmail.com>
2023-04-02 17:12:25 +0200

    refactor: simplify iban search logic and remove unused import statement

--- src/routers/features/idpay/iban.ts
+++ src/routers/features/idpay/iban.ts
@@ -1,6 +1,5 @@
 import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
-import { getIbanResponse } from "../../../payloads/features/idpay/iban/get-iban";
 import { getIbanListResponse } from "../../../payloads/features/idpay/iban/get-iban-list";
 import { addIdPayHandler } from "./router";
 
@@ -8,16 +7,11 @@ addIdPayHandler("get", "/iban/:iban", (req, res) =>
   pipe(
     O.fromNullable(req.params.iban),
     O.chain(iban =>
-      getIbanListResponse.ibanList.some(i => i.iban === iban)
-        ? O.some(iban)
-        : O.none
+      O.fromNullable(getIbanListResponse.ibanList.find(i => i.iban === iban))
     ),
     O.fold(
       () => res.sendStatus(400),
-      iban =>
-        res
-          .status(200)
-          .json(getIbanListResponse.ibanList.find(i => i.iban === iban))
+      iban => res.status(200).json(iban)
     )
   )
 );
