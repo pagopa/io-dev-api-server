7e92288da9b0769c06e15b75e2a22f0b9f2f3a1a
Federico Mastrini <federicomastrini93@gmail.com>
2023-04-04 16:31:50 +0200

    chore: added IBAN endpoint tests

---
+++ src/routers/features/idpay/__tests__/iban.test.ts
@@ -0,0 +1,30 @@
+import supertest from "supertest";
+import { ibanList } from "../../../../payloads/features/idpay/iban/data";
+import { getIbanListResponse } from "../../../../payloads/features/idpay/iban/get-iban-list";
+import app from "../../../../server";
+import { addIdPayPrefix } from "../router";
+
+const request = supertest(app);
+
+describe("IDPay IBAN API", () => {
+  describe("GET getIbanList", () => {
+    it("should return 200 with IBAN list", async () => {
+      const response = await request.get(addIdPayPrefix("/iban"));
+      expect(response.status).toBe(200);
+      expect(response.body).toStrictEqual(getIbanListResponse);
+    });
+  });
+  describe("GET getIban", () => {
+    it("should return 200 with the requested IBAN if IBAN exists", async () => {
+      const iban = ibanList[0];
+
+      const response = await request.get(addIdPayPrefix(`/iban/${iban.iban}`));
+      expect(response.status).toBe(200);
+      expect(response.body).toStrictEqual(iban);
+    });
+    it("should return 404 if IBAN does not exist", async () => {
+      const response = await request.get(addIdPayPrefix(`/iban/ABC123`));
+      expect(response.status).toBe(404);
+    });
+  });
+});
