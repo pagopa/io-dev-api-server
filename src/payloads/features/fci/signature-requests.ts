020e8b4a9c4a4cf49655a7ae83d5d8cd1bd9e862
Mario Perrotta <mario.perrotta@pagopa.it>
2023-04-05 14:13:12 +0200

    chore: update

--- src/payloads/features/fci/signature-requests.ts
+++ src/payloads/features/fci/signature-requests.ts
@@ -1,9 +1,6 @@
-import { EmailString, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import faker from "faker/locale/it";
-import { SignatureRequestDetailView } from "../../../../generated/definitions/fci/SignatureRequestDetailView";
 import { SignatureRequestStatusEnum } from "../../../../generated/definitions/fci/SignatureRequestStatus";
 import { DossierTitle } from "../../../../generated/definitions/fci/DossierTitle";
-import { serverUrl } from "../../../utils/server";
 import { SignatureRequestListView } from "../../../../generated/definitions/fci/SignatureRequestListView";
 import { getRandomEnumValue } from "../../utils/random";
 import { SignatureRequestList } from "../../../../generated/definitions/fci/SignatureRequestList";
@@ -11,18 +8,18 @@ import { SignatureRequestList } from "../../../../generated/definitions/fci/Sign
 const now = new Date();
 
 const createRandomSignatureRequest = (): SignatureRequestListView => ({
-  id: faker.datatype.uuid() as SignatureRequestDetailView["id"],
+  id: faker.datatype.uuid() as SignatureRequestListView["id"],
   status: getRandomEnumValue(SignatureRequestStatusEnum),
   created_at: new Date(),
-  dossier_id: faker.datatype.uuid() as SignatureRequestDetailView["dossier_id"],
+  dossier_id: faker.datatype.uuid() as SignatureRequestListView["dossier_id"],
   dossier_title: faker.random.words(5) as DossierTitle,
   expires_at: new Date(now.setDate(now.getDate() + 30)),
-  signer_id: faker.datatype.uuid() as SignatureRequestDetailView["signer_id"],
+  signer_id: faker.datatype.uuid() as SignatureRequestListView["signer_id"],
   updated_at: new Date()
 });
 
 export const signatureRequestList: SignatureRequestList = {
   items: Array.from({ length: 5 }, () =>
     createRandomSignatureRequest()
-  ) as SignatureRequestListView[]
+  ) as Array<SignatureRequestListView>
 };
