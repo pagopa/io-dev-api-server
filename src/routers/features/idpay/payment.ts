866ed059fcc71d9a890a35bf9b3ce90ef9692fb9
Federico Mastrini <federicomastrini93@gmail.com>
2023-06-16 11:23:36 +0200

    chore: payment failures

--- src/routers/features/idpay/payment.ts
+++ src/routers/features/idpay/payment.ts
@@ -1,9 +1,59 @@
+import { faker } from "@faker-js/faker";
 import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/function";
-import { getIdPayError } from "../../../payloads/features/idpay/error";
-import { putAuthPaymentResponse } from "../../../payloads/features/idpay/put-auth-payment";
-import { putPreAuthPaymentResponse } from "../../../payloads/features/idpay/put-pre-auth-payment";
+import { ulid } from "ulid";
+import {
+  AuthPaymentResponseDTO,
+  StatusEnum as PaymentStatusEnum
+} from "../../../../generated/definitions/idpay/AuthPaymentResponseDTO";
+import {
+  CodeEnum,
+  TransactionErrorDTO
+} from "../../../../generated/definitions/idpay/TransactionErrorDTO";
+import { getRandomEnumValue } from "../../../payloads/utils/random";
 import { addIdPayHandler } from "./router";
+
+const generateRandomAuthPaymentResponseDTO = (): AuthPaymentResponseDTO => {
+  const amount = faker.datatype.number({
+    min: 100,
+    max: 10000
+  });
+
+  return {
+    id: ulid(),
+    initiativeId: ulid(),
+    status: getRandomEnumValue(PaymentStatusEnum),
+    trxCode: faker.datatype.string(),
+    reward: amount,
+    amountCents: amount,
+    businessName: faker.commerce.productName(),
+    initiativeName: faker.company.name(),
+    trxDate: faker.date.recent(0),
+    residualBudget: faker.datatype.number({
+      min: 1000,
+      max: 20000
+    })
+  };
+};
+
+const generateRandomTransactionError = (
+  code: CodeEnum
+): TransactionErrorDTO => ({
+  code,
+  message: faker.lorem.sentence()
+});
+
+const codeToFailure: {
+  [key: number]: { status: number; code: CodeEnum };
+} = {
+  1: { status: 404, code: CodeEnum.PAYMENT_NOT_FOUND_EXPIRED },
+  2: { status: 403, code: CodeEnum.PAYMENT_USER_NOT_VALID },
+  3: { status: 400, code: CodeEnum.PAYMENT_STATUS_NOT_VALID },
+  4: { status: 403, code: CodeEnum.PAYMENT_BUDGET_EXHAUSTED },
+  5: { status: 403, code: CodeEnum.PAYMENT_GENERIC_REJECTED },
+  6: { status: 429, code: CodeEnum.PAYMENT_TOO_MANY_REQUESTS },
+  7: { status: 500, code: CodeEnum.PAYMENT_GENERIC_ERROR }
+};
 
 /**
  * Pre Authorize payment
@@ -12,10 +62,14 @@ addIdPayHandler("put", "/payment/qr-code/:trxCode/relate-user", (req, res) =>
   pipe(
     req.params.trxCode,
     O.fromNullable,
-    O.chain(putPreAuthPaymentResponse),
+    O.map(trxCode => trxCode[trxCode.length - 2]),
+    O.map(parseInt),
+    O.filter(code => code <= 7),
+    O.chain(code => O.fromNullable(codeToFailure[code])),
     O.fold(
-      () => res.status(404).json(getIdPayError(404)),
-      data => res.status(200).json(data)
+      () => res.status(200).json(generateRandomAuthPaymentResponseDTO()),
+      ({ status, code }) =>
+        res.status(status).json(generateRandomTransactionError(code))
     )
   )
 );
@@ -27,10 +81,14 @@ addIdPayHandler("put", "/payment/qr-code/:trxCode/authorize", (req, res) =>
   pipe(
     req.params.trxCode,
     O.fromNullable,
-    O.chain(putAuthPaymentResponse),
+    O.map(trxCode => trxCode[trxCode.length - 1]),
+    O.map(parseInt),
+    O.filter(code => code <= 7),
+    O.chain(code => O.fromNullable(codeToFailure[code])),
     O.fold(
-      () => res.status(404).json(getIdPayError(404)),
-      data => res.status(200).json(data)
+      () => res.status(200).json(generateRandomAuthPaymentResponseDTO()),
+      ({ status, code }) =>
+        res.status(status).json(generateRandomTransactionError(code))
     )
   )
 );
