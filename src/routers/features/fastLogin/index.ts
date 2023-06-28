d543e6ddecf4b5996b21d8a1bb367db2778b049c
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2023-06-28 10:39:36 +0200

    chore: Add fast-login api Open API specs

--- src/routers/features/fastLogin/index.ts
+++ src/routers/features/fastLogin/index.ts
@@ -5,6 +5,7 @@
 import { Router } from "express";
 import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
+import * as E from "fp-ts/lib/Either";
 import { addHandler } from "../../../payloads/response";
 import {
   generateNewNonce,
@@ -13,6 +14,7 @@ import {
 import { refreshTokenWithFastLogin } from "../../../features/fastLogin/fastLoginHandler";
 import { lollipopMiddleware } from "../../../middleware/lollipopMiddleware";
 import { addApiV1Prefix } from "../../../utils/strings";
+import { FastLoginResponse } from "../../../../generated/definitions/fast_login/FastLoginResponse";
 
 export const fastLoginRouter = Router();
 
@@ -37,7 +39,14 @@ addHandler(
       O.fromNullable,
       O.fold(
         () => res.status(401),
-        token => res.status(200).send({ token })
+        token =>
+          pipe(
+            FastLoginResponse.decode({ token }),
+            E.fold(
+              () => res.status(403),
+              response => res.status(200).send(response)
+            )
+          )
       )
     )
   )
