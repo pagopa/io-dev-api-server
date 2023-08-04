24d6cc9680edb42dcdb2d6ed6124921cba673a7e
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2023-08-04 14:13:42 +0200

    chore: Improve type safety

--- src/middleware/fastLoginMiddleware.ts
+++ src/middleware/fastLoginMiddleware.ts
@@ -3,6 +3,12 @@ import { ProblemJson } from "@pagopa/ts-commons/lib/responses";
 import { isSessionTokenValid } from "../persistence/sessionInfo";
 import { ErrorDTO } from "../../generated/definitions/idpay/ErrorDTO";
 
+type FastLoginMiddleware401Response = ErrorDTO & ProblemJson;
+const responseFor401: FastLoginMiddleware401Response = {
+  code: 401,
+  message: "Unauthorized"
+};
+
 /**
  * if the response error is defined as a global config
  * this middleware returns an error from those ones defined
@@ -20,8 +26,5 @@ export const fastLoginMiddleware = (
     next();
     return;
   }
-  res.status(401).send({
-    code: 401,
-    message: "Unauthorized"
-  } as ErrorDTO & ProblemJson);
+  res.status(401).send(responseFor401);
 };
