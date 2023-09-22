04f57822567b0c806ded0b96f9e4d1f95c9c52d9
Federico Mastrini <federicomastrini93@gmail.com>
2023-09-22 11:32:22 +0200

    fix: `nInstr`

--- src/payloads/features/idpay/get-wallet-detail.ts
+++ src/payloads/features/idpay/get-wallet-detail.ts
@@ -1,7 +1,11 @@
 import * as O from "fp-ts/lib/Option";
 import { InitiativeDTO } from "../../../../generated/definitions/idpay/InitiativeDTO";
-import { initiatives } from "../../../persistence/idpay";
+import { initiatives, instruments } from "../../../persistence/idpay";
 
 export const getWalletDetailResponse = (
   initiativeId: string
-): O.Option<InitiativeDTO> => O.fromNullable(initiatives[initiativeId]);
+): O.Option<InitiativeDTO> =>
+  O.fromNullable<InitiativeDTO>({
+    ...initiatives[initiativeId],
+    nInstr: instruments[initiativeId].length
+  });
