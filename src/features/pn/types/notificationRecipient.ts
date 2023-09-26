fb4ac0d4ae9afaa1035985622dbfa5cbec165676
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2023-09-26 17:28:30 +0200

    update type

--- src/features/pn/types/notificationRecipient.ts
+++ src/features/pn/types/notificationRecipient.ts
@@ -3,5 +3,6 @@ import { NotificationPaymentInfo } from "./notificationPaymentInfo";
 export type NotificationRecipient = {
   taxId: string;
   denomination: string;
+  recipientType: string;
   payment: NotificationPaymentInfo;
 };
