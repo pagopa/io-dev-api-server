8c34b748ed3de4edf0be70ebfaf813f651be5025
Alessandro Izzo <alessandro.izzo@pagopa.it>
2023-10-23 15:49:37 +0200

    chore: added new apis to handle new wallet onboarding mocks

--- src/routers/features/walletV3/onboarding/index.ts
+++ src/routers/features/walletV3/onboarding/index.ts
@@ -1,6 +1,7 @@
 import { pipe } from "fp-ts/lib/function";
 import * as E from "fp-ts/lib/Either";
 
+import WalletDB from "../../../../persistence/wallet";
 import { WalletCreateRequest } from "../../../../../generated/definitions/pagopa/walletv3/WalletCreateRequest";
 import { generateOnboardingWalletData } from "../../../../utils/wallet";
 import { addPaymentMethodsHandler, addWalletV3Handler } from "../router";
@@ -19,9 +20,22 @@ addWalletV3Handler("post", "/", (req, res) => {
   );
 });
 
+addWalletV3Handler("get", "/", (req, res) => {
+  res.json(WalletDB.getUserWallets());
+});
+
 /**
  * This API is used to retrieve a list of payment methods available for the onboarding process
  */
 addPaymentMethodsHandler("get", "/", (req, res) => {
   res.json(generateOnboardablePaymentMethods());
+});
+
+/**
+ * This API is used to start an onboarding process for a new method of payment
+ */
+addWalletV3Handler("post", "/mock", (req, res) => {
+  const { paymentMethodId } = req.body;
+  const generatedWallet = WalletDB.generateUserWallet(paymentMethodId);
+  res.json(generatedWallet);
 });
