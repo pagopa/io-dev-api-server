51d8ef3641a8ec27e69d7d05eed147fdfb12957d
Federico Mastrini <federicomastrini93@gmail.com>
2023-11-20 17:54:14 +0100

    chore: psps endpoint

--- src/features/wallet/payment/payloads/transactions.ts
+++ src/features/wallet/payment/payloads/transactions.ts
@@ -1,12 +1,14 @@
 import { faker } from "@faker-js/faker/locale/it";
 import * as O from "fp-ts/lib/Option";
 import { ulid } from "ulid";
+import { CalculateFeeResponse } from "../../../../../generated/definitions/pagopa/ecommerce/CalculateFeeResponse";
 import {
   ClientIdEnum,
   NewTransactionResponse,
   SendPaymentResultOutcomeEnum
 } from "../../../../../generated/definitions/pagopa/ecommerce/NewTransactionResponse";
 import { PaymentInfo } from "../../../../../generated/definitions/pagopa/ecommerce/PaymentInfo";
+import { PaymentMethodStatusEnum } from "../../../../../generated/definitions/pagopa/ecommerce/PaymentMethodStatus";
 import { RptId } from "../../../../../generated/definitions/pagopa/ecommerce/RptId";
 import { TransactionInfo } from "../../../../../generated/definitions/pagopa/ecommerce/TransactionInfo";
 import { TransactionStatusEnum } from "../../../../../generated/definitions/pagopa/ecommerce/TransactionStatus";
@@ -18,7 +20,6 @@ export const getNewTransactionResponsePayload = (
     transactionId: ulid(),
     payments,
     status: TransactionStatusEnum.ACTIVATED,
-    authorizationCode: faker.datatype.string(10),
     clientId: ClientIdEnum.IO,
     sendPaymentResultOutcome: SendPaymentResultOutcomeEnum.OK
   });
@@ -40,4 +41,37 @@ export const getTransactionInfoPayload = (
     status: TransactionStatusEnum.ACTIVATED,
     clientId: ClientIdEnum.IO,
     sendPaymentResultOutcome: SendPaymentResultOutcomeEnum.OK
+  });
+
+export const getCalculateFeeResponsePayload = (
+  _walletId: string,
+  _amount: number
+): O.Option<CalculateFeeResponse> =>
+  O.some({
+    paymentMethodName: "VISA",
+    paymentMethodDescription: "Test",
+    paymentMethodStatus: PaymentMethodStatusEnum.ENABLED,
+    bundles: [
+      {
+        abi: "01010",
+        taxPayerFee: 123,
+        primaryCiIncurredFee: 123,
+        bundleName: "BANCO di NAPOLI",
+        idBundle: "A"
+      },
+      {
+        abi: "01015",
+        bundleName: "Banco di Sardegna",
+        taxPayerFee: 456,
+        primaryCiIncurredFee: 456,
+        idBundle: "B"
+      },
+      {
+        abi: "03015",
+        bundleName: "FINECO",
+        taxPayerFee: 789,
+        primaryCiIncurredFee: 789,
+        idBundle: "C"
+      }
+    ]
   });
