756d3d6abc43223f8ca0a85dafe79f5b298e7632
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2023-11-24 13:05:54 +0100

    chore: prettify

--- src/utils/responseTypes.ts
+++ src/utils/responseTypes.ts
@@ -1,9 +1,9 @@
 export type CustomResponse = {
-    status: number;
-    payload?: object;
-  };
+  status: number;
+  payload?: object;
+};
 
 export type ResponseProblem<T extends string> = {
-    status: number;
-    detail: T;
-  };
+  status: number;
+  detail: T;
+};
