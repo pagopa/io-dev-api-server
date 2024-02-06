18a941e1e9602d6fb5b2b9cdccf2372c81500280
Federico Mastrini <federicomastrini93@gmail.com>
2024-02-06 12:00:04 +0100

    chore: completely remove BV, SV and Cashback

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -9,33 +9,6 @@ import {
   WAIT_QTSP_SIGNATURE_REQUEST_ID
 } from "../payloads/features/fci/signature-request";
 
-export const frontMatter2CTA = `---
-it:
-  cta_1:
-    text: "Attiva il cashback"
-    action: "ioit://CTA_START_BPD"
-en:
-  cta_1:
-    text: "Request cashback"
-    action: "ioit://CTA_START_BPD"
----
-Aderisci al “Cashback” tramite la sezione **Portafoglio** dell’app IO: puoi ottenere un rimborso sugli acquisti effettuati con carte, bancomat e app di pagamento. Il “Cashback” è una delle iniziative del Piano Italia Cashless promosso dal Governo allo scopo di incentivare un maggiore utilizzo di moneta elettronica nel Paese.
-#### Chi può richiederlo?
-Se hai compiuto i 18 anni e risiedi in Italia, puoi ottenere un **rimborso in denaro** a fronte di acquisti personali e non per uso professionale con i tuoi **metodi di pagamento elettronici** presso **punti vendita fisici** (non online) situati sul territorio nazionale.
-#### Come funziona il Cashback?
-1. Il programma Cashback si divide in periodi di durata variabile. Il primo periodo **sperimentale** detto **“Extra Cashback di Natale”**, dura **dall'8 al 31 dicembre 2020.** I successivi dureranno 6 mesi ciascuno, a partire dal 1° gennaio 2021.
-2. Per ogni periodo potrai ottenere un **rimborso massimo di €150**. Ogni acquisto effettuato con strumenti di pagamento elettronici **registrati** ai fini dell’iniziativa, ti farà accumulare il 10% dell’importo speso, fino ad un massimo di €15 per transazione.
-3. Il cashback accumulato ti verrà rimborsato solo se avrai raggiunto il numero minimo di transazioni valide: **10 nel periodo sperimentale**, 50 in ciascuno dei semestri successivi.
-4. Oltre al Cashback, **ma solo a partire dal 1° gennaio 2021**, **i primi 100mila** partecipanti che in ogni semestre hanno totalizzato il **maggior numero di transazioni valide**, ricevono un **Super Cashback di €1500**.
-5. Al termine del periodo, ricevi il rimborso complessivo accumulato **sull’IBAN che indicherai durante l’attivazione.**
-#### Come si aggiungono i metodi di pagamento?
-Aggiungi subito i metodi **a te intestati** nella sezione [Portafoglio](ioit://WALLET_HOME), e abilitali al cashback quando richiesto. Ad oggi sono supportate carte di debito, credito, prepagate e PagoBANCOMAT. Stiamo lavorando per supportare altri metodi in futuro, come Bancomat Pay e Satispay. 
-#### Come si aggiunge l'IBAN per ricevere il rimborso previsto?
-Attiva il Cashback e inserisci l’IBAN quando richiesto. Puoi inserirlo anche in un secondo momento, ma ricordati di farlo entro il termine del periodo per avere diritto al rimborso.
-Per poter attivare il Cashback, devi avere aggiornato IO all'ultima versione disponibile. Scaricala adesso!
-[App Store](https://apps.apple.com/it/app/io/id1501681835)
-[Play Store](https://play.google.com/store/apps/details?id=it.pagopa.io.app)`;
-
 export const frontMatterMyPortal = `---
 it:
     cta_1: 
@@ -53,23 +26,6 @@ en:
         action: "ioit://WALLET_HOME"
 ---`;
 
-export const frontMatterBonusVacanze = `---
-it:
-    cta_1: 
-        text: "start eligibility"
-        action: "ioit://BONUS_CTA_ELIGILITY_START"
-    cta_2: 
-        text: "bonus list"
-        action: "ioit://BONUS_AVAILABLE_LIST"
-en:
-    cta_1: 
-        text: "start eligibility"
-        action: "ioit://BONUS_CTA_ELIGILITY_START"
-    cta_2: 
-        text: "bonus list"
-        action: "ioit://BONUS_AVAILABLE_LIST"
----`;
-
 export const frontMatter2CTA2 = `---
 it:
     cta_1: 
@@ -85,28 +41,6 @@ en:
     cta_2: 
         text: "internal webview"
         action: "ioit://SERVICE_WEBVIEW?url=https://www.google.com"
----`;
-
-export const frontMatter1CTABonusBpd = `---
-it:
-    cta_1: 
-        text: "BPD start"
-        action: "ioit://CTA_START_BPD"
-en:
-    cta_1: 
-        text: "BPD start EN"
-        action: "ioit://CTA_BPD_IBAN_EDIT"
----`;
-
-export const frontMatter1CTABonusBpdIban = `---
-it:
-    cta_1: 
-        text: "BPD Iban"
-        action: "ioit://CTA_BPD_IBAN_EDIT"
-en:
-    cta_1: 
-        text: "BPD Iban EN"
-        action: "ioit://CTA_BPD_IBAN_EDIT"
 ---`;
 
 export const frontMatterInvalid = `---
