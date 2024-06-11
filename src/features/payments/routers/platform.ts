e904764120d34a698d59f2e8ef9c1e1439ce5265
Alessandro Izzo <alessandro.izzo998@gmail.com>
2024-06-11 21:28:33 +0200

    chore: Add generic custom token to follow the definitions regex

--- src/features/payments/routers/platform.ts
+++ src/features/payments/routers/platform.ts
@@ -1,7 +1,7 @@
-import { ulid } from "ulid";
+import { PAGOPA_PLATFORM_SESSION_TOKEN } from "../utils/platform";
 import { addPlatformHandler } from "./router";
 
 // Generates a new pagoPA platform session token
 addPlatformHandler("post", "/session", (_, res) =>
-  res.status(201).json({ token: ulid() })
+  res.status(201).json({ token: PAGOPA_PLATFORM_SESSION_TOKEN })
 );
