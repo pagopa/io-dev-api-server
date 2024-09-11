fe2ba560785afaf8dfaed9f8dd0443e5d444b02a
Frederik Batuna <gobi301@gmail.com>
2024-09-11 10:25:08 +0200

    fix: formatting

--- src/routers/__tests__/json-mock.test.ts
+++ src/routers/__tests__/json-mock.test.ts
@@ -3,5 +3,7 @@ import { readFileAsJSON } from "../../utils/file";
 
 // test added beacuse CI did not catch malformed json file
 it("should return bonus_available_v2.json", async () => {
-  await readFileAsJSON(assetsFolder + "/bonus_available/bonus_available_v2.json");
+  await readFileAsJSON(
+    assetsFolder + "/bonus_available/bonus_available_v2.json"
+  );
 });
