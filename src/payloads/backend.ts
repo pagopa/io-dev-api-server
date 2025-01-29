2d26f1a5fe3efe36c090282cad63fd6b6e2ca435
ChrisMattew <cristian.matteu@spindox.it>
2025-01-29 17:30:02 +0100

    chore: remove cieid ff from backend.ts

--- src/payloads/backend.ts
+++ src/payloads/backend.ts
@@ -136,12 +136,6 @@ export const backendStatus: BackendStatus = {
         }
       }
     },
-    cie_id: {
-      min_app_version: {
-        ios: "0.0.0.0",
-        android: "0.0.0.0"
-      }
-    },
     emailUniquenessValidation: {
       min_app_version: {
         ios: "0.0.0",
