fc38c3f62b7adb1ab6b33d5a398e059e880cb7e6
ChrisMattew <cristian.matteu@spindox.it>
2025-02-19 14:52:16 +0100

    feat: upgrade IO_SERVICES_METADATA_VERSION

--- scripts/generate-api-models.sh
+++ scripts/generate-api-models.sh
@@ -2,12 +2,12 @@
 
 IO_BACKEND_VERSION=v16.7.4-RELEASE
 # need to change after merge on io-services-metadata
-IO_SERVICES_METADATA_VERSION=1.0.58
+IO_SERVICES_METADATA_VERSION=1.0.59
 
 declare -a noParams=(
   "./generated/definitions/backend https://raw.githubusercontent.com/pagopa/io-backend/$IO_BACKEND_VERSION/api_public.yaml"
   "./generated/definitions/session_manager https://raw.githubusercontent.com/pagopa/io-auth-n-identity-domain/io-session-manager@1.0.0/apps/io-session-manager/api/public.yaml"
-  "./generated/definitions/content https://raw.githubusercontent.com/pagopa/io-services-metadata/refs/heads/feature/content-educational-zendesk-subcategories/definitions.yml"
+  "./generated/definitions/content https://raw.githubusercontent.com/pagopa/io-services-metadata/$IO_SERVICES_METADATA_VERSION/definitions.yml"
   "./generated/definitions/pagopa/cobadge/configuration https://raw.githubusercontent.com/pagopa/io-services-metadata/$IO_SERVICES_METADATA_VERSION/pagopa/cobadge/abi_definitions.yml"
   "./generated/definitions/pagopa/privative/configuration https://raw.githubusercontent.com/pagopa/io-services-metadata/$IO_SERVICES_METADATA_VERSION/pagopa/privative/definitions.yml"
   "./generated/definitions/pagopa/walletv2 https://raw.githubusercontent.com/pagopa/io-services-metadata/$IO_SERVICES_METADATA_VERSION/bonus/specs/bpd/pm/walletv2.json"
