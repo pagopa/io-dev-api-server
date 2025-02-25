b4c22a8611c75d7c48cb7f98c232c657249c30d3
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-02-25 15:30:32 +0100

    feat: Adds the status endpoint

--- src/routers/misc.ts
+++ src/routers/misc.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { ioDevServerConfig } from "../config";
 import { addHandler } from "../payloads/response";
 import { sendFileFromRootPath } from "../utils/file";
+import { addApiV1Prefix } from "../utils/strings";
 
 export const miscRouter = Router();
 
@@ -15,4 +16,12 @@ addHandler(miscRouter, "get", "/myportal_playground.html", (_, res) => {
  */
 addHandler(miscRouter, "get", "/config", (_, res) => {
   res.json(ioDevServerConfig);
+});
+
+/**
+ * API dedicated to status
+ * return the current server status
+ */
+addHandler(miscRouter, "get", addApiV1Prefix("/status"), (_, res) => {
+  res.sendStatus(204);
 });
