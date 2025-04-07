2589d158b52855c130892e8374f3426edc67a22c
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2025-04-07 17:02:15 +0200

    wip

--- src/features/services/routers/services.ts
+++ src/features/services/routers/services.ts
@@ -9,6 +9,29 @@ import { addApiV2Prefix, serviceRouter } from "./router";
 
 const serviceConfig = ioDevServerConfig.features.service;
 
+// Retrieve featured services
+addHandler(
+  serviceRouter,
+  "get",
+  addApiV2Prefix("/services/featured"),
+  (_, res) =>
+    pipe(
+      serviceConfig.response.featuredServicesResponseCode,
+      O.fromPredicate(statusCode => statusCode !== 200),
+      O.fold(
+        () =>
+          pipe(
+            O.of(getFeaturedServicesResponsePayload()),
+            O.fold(
+              () => res.status(500),
+              featuredServices => res.status(200).json(featuredServices)
+            )
+          ),
+        statusCode => res.sendStatus(statusCode)
+      )
+    )
+);
+
 // Retrive service by id
 addHandler(
   serviceRouter,
@@ -29,29 +52,6 @@ addHandler(
             O.fold(
               () => res.sendStatus(404),
               service => res.status(200).json(service)
-            )
-          ),
-        statusCode => res.sendStatus(statusCode)
-      )
-    )
-);
-
-// Retrieve featured services
-addHandler(
-  serviceRouter,
-  "get",
-  addApiV2Prefix("/services/featured"),
-  (_, res) =>
-    pipe(
-      serviceConfig.response.featuredServicesResponseCode,
-      O.fromPredicate(statusCode => statusCode !== 200),
-      O.fold(
-        () =>
-          pipe(
-            O.of(getFeaturedServicesResponsePayload()),
-            O.fold(
-              () => res.status(404),
-              featuredServices => res.status(200).json(featuredServices)
             )
           ),
         statusCode => res.sendStatus(statusCode)
