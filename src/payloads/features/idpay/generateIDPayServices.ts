0d3336826ea565b7735cda35af3ca30b48a489f3
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-07-21 15:59:38 +0200

    feat: idpay service to activate message notifications

---
+++ src/payloads/features/idpay/generateIDPayServices.ts
@@ -0,0 +1,24 @@
+import {
+  NonEmptyString,
+  OrganizationFiscalCode
+} from "@pagopa/ts-commons/lib/strings";
+import { ScopeTypeEnum } from "../../../../generated/definitions/services/ScopeType";
+import { ServiceDetails } from "../../../../generated/definitions/services/ServiceDetails";
+import {
+  createServiceFromFactory,
+  createServiceMetadataFromFactory
+} from "../../../features/services/persistence/services/factory";
+import { IDPayServiceID } from "./types";
+
+export const generateIDPayServices = (): ServiceDetails[] =>
+  Object.values(IDPayServiceID).map(serviceId => ({
+    ...createServiceFromFactory(`TESTSRV${serviceId}` as string),
+    organization: {
+      fiscal_code: "00000009898" as OrganizationFiscalCode,
+      name: `TESTSRV${serviceId}` as NonEmptyString
+    },
+    metadata: {
+      ...createServiceMetadataFromFactory(ScopeTypeEnum.NATIONAL)
+    },
+    name: `TESTSRV${serviceId}` as NonEmptyString
+  }));
