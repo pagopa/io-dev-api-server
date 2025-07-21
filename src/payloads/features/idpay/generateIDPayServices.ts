d0df7b4f57cbfeba89262644547d0148bbec7b42
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-07-21 18:08:06 +0200

    fix: pad initiatives before 10

--- src/payloads/features/idpay/generateIDPayServices.ts
+++ src/payloads/features/idpay/generateIDPayServices.ts
@@ -2,6 +2,7 @@ import {
   NonEmptyString,
   OrganizationFiscalCode
 } from "@pagopa/ts-commons/lib/strings";
+import _ from "lodash";
 import { ScopeTypeEnum } from "../../../../generated/definitions/services/ScopeType";
 import { ServiceDetails } from "../../../../generated/definitions/services/ServiceDetails";
 import {
@@ -10,19 +11,22 @@ import {
 } from "../../../features/services/persistence/services/factory";
 import { IDPayServiceID } from "./types";
 
-const serviceIds = Object.values(IDPayServiceID).filter(
-  v => typeof v === "number"
-) as number[];
+const serviceIds = _.pickBy(IDPayServiceID, (value, key) => isNaN(Number(key)));
 
 export const generateIDPayServices = (): ServiceDetails[] =>
-  serviceIds.map((serviceId, index) => ({
-    ...createServiceFromFactory(`TESTSRV${serviceId}` as string),
-    organization: {
-      fiscal_code: index.toString().padStart(11, "0") as OrganizationFiscalCode,
-      name: `TESTSRV${serviceId}` as NonEmptyString
-    },
-    metadata: {
-      ...createServiceMetadataFromFactory(ScopeTypeEnum.NATIONAL)
-    },
-    name: `TESTSRV${serviceId}` as NonEmptyString
-  }));
+  Object.values(serviceIds).map((serviceId, index) => {
+    const paddedId = serviceId.toString().padStart(2, "0");
+    return {
+      ...createServiceFromFactory(`TESTSRV${paddedId}` as string),
+      organization: {
+        fiscal_code: index
+          .toString()
+          .padStart(11, "0") as OrganizationFiscalCode,
+        name: `TESTSRV${paddedId}` as NonEmptyString
+      },
+      metadata: {
+        ...createServiceMetadataFromFactory(ScopeTypeEnum.NATIONAL)
+      },
+      name: `TESTSRV${paddedId}` as NonEmptyString
+    };
+  });
