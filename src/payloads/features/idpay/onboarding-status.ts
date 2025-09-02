80f0e075e36cbe31eafcea1c03f5fba8aa9dbc91
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-09-02 17:47:19 +0200

    chore: removed wrong enum

--- src/payloads/features/idpay/onboarding-status.ts
+++ src/payloads/features/idpay/onboarding-status.ts
@@ -36,10 +36,6 @@ const onboardingStatuses: {
   [IDPayInitiativeID.KO_STATUS_ON_EVALUATION]: {
     status: OnboardingStatusEnum.ON_EVALUATION,
     statusDate: faker.date.recent({ days: 1 })
-  },
-  [IDPayInitiativeID.KO_STATUS_TOO_MANY_REQUESTS]: {
-    status: OnboardingStatusEnum.TOO_MANY_REQUESTS,
-    statusDate: faker.date.recent({ days: 1 })
   }
 };
 
