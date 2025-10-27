e83a6774bc939a66e18d9f9f44faab2bba30bb11
ChrisMattew <cristian.matteu@spindox.it>
2025-10-27 17:19:27 +0100

    feat: add AAR phase 3 FF config

--- scripts/generate-api-models.sh
+++ scripts/generate-api-models.sh
@@ -9,7 +9,7 @@ IO_SESSION_MANAGER_VERSION=1.8.0
 declare -a noParams=(
   "./generated/definitions/backend https://raw.githubusercontent.com/pagopa/io-backend/$IO_BACKEND_VERSION/api_public.yaml"
   "./generated/definitions/session_manager https://raw.githubusercontent.com/pagopa/io-auth-n-identity-domain/io-session-manager@$IO_SESSION_MANAGER_VERSION/apps/io-session-manager/api/external.yaml"
-  "./generated/definitions/content https://raw.githubusercontent.com/pagopa/io-services-metadata/$IO_SERVICES_METADATA_VERSION/definitions.yml"
+  "./generated/definitions/content https://raw.githubusercontent.com/pagopa/io-services-metadata/refs/heads/IOCOM-2708-ff-aar-flow-phase-3/definitions.yml"
   "./generated/definitions/pagopa/cobadge/configuration https://raw.githubusercontent.com/pagopa/io-services-metadata/$IO_SERVICES_METADATA_VERSION/pagopa/cobadge/abi_definitions.yml"
   "./generated/definitions/pagopa/privative/configuration https://raw.githubusercontent.com/pagopa/io-services-metadata/$IO_SERVICES_METADATA_VERSION/pagopa/privative/definitions.yml"
   "./generated/definitions/pagopa/walletv2 https://raw.githubusercontent.com/pagopa/io-services-metadata/$IO_SERVICES_METADATA_VERSION/bonus/specs/bpd/pm/walletv2.json"
