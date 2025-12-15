465794c2655f8f59c41cfffff9b482a1c6b66b00
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-12-15 18:30:06 +0100

    chore: hide services based on idpay flag (#566)

--- src/payloads/features/idpay/generateIDPayServices.ts
+++ src/payloads/features/idpay/generateIDPayServices.ts
@@ -9,24 +9,32 @@ import {
   createServiceFromFactory,
   createServiceMetadataFromFactory
 } from "../../../features/services/persistence/services/factory";
+import { backendStatus } from "../../backend";
 import { IDPayServiceID } from "./types";
 
 const serviceIds = _.pickBy(IDPayServiceID, (value, key) => isNaN(Number(key)));
 
-export const generateIDPayServices = (): ServiceDetails[] =>
-  Object.values(serviceIds).map((serviceId, index) => {
-    const paddedId = serviceId.toString().padStart(2, "0");
-    return {
-      ...createServiceFromFactory(`TESTSRV${paddedId}` as string),
-      organization: {
-        fiscal_code: index
-          .toString()
-          .padStart(11, "0") as OrganizationFiscalCode,
-        name: `TESTSRV${paddedId}` as NonEmptyString
-      },
-      metadata: {
-        ...createServiceMetadataFromFactory(ScopeTypeEnum.NATIONAL)
-      },
-      name: `TESTSRV${paddedId}` as NonEmptyString
-    };
-  });
+// IDPay services are only generated if the onboarding flag is enabled in backend config
+export const generateIDPayServices = (): ServiceDetails[] => {
+  const isIdPayEnabled =
+    backendStatus.config.idPay?.onboarding?.enabled ?? false;
+
+  return isIdPayEnabled
+    ? Object.values(serviceIds).map((serviceId, index) => {
+        const paddedId = serviceId.toString().padStart(2, "0");
+        return {
+          ...createServiceFromFactory(`TESTSRV${paddedId}` as string),
+          organization: {
+            fiscal_code: index
+              .toString()
+              .padStart(11, "0") as OrganizationFiscalCode,
+            name: `TESTSRV${paddedId}` as NonEmptyString
+          },
+          metadata: {
+            ...createServiceMetadataFromFactory(ScopeTypeEnum.NATIONAL)
+          },
+          name: `TESTSRV${paddedId}` as NonEmptyString
+        };
+      })
+    : [];
+};
